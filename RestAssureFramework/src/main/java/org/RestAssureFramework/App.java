87843a7363282ec7adc1737f3ecfe2fe79363024
Kapil Jain <kjain005@marriott.com>
2018-08-16 18:17:26 -0400

    Added Modules to the Project

---
+++ RestAssureFramework/src/main/java/org/RestAssureFramework/App.java
@@ -0,0 +1,13 @@
+package org.RestAssureFramework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Service-Mock/src/main/java/org/Service/Mock/App.java
@@ -0,0 +1,13 @@
+package org.Service.Mock;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
